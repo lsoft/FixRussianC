6f4f145001c618feb92a31962fd12fac7e65f756
lsoft <lsoft@list.ru>
2022-09-29 22:30:01 +0500

    norefs Improved regex (exlude patterns like \rСолнце)

--- FixRussianC.Shared/FixRussianC/Tag/RussianCTagger.cs
+++ FixRussianC.Shared/FixRussianC/Tag/RussianCTagger.cs
@@ -9,7 +9,7 @@ namespace FixRussianC2019.FixRussianC.Tag
         public RussianCTagger(ITextBuffer buffer)
             : base(buffer, new[]
             {
-                new Regex(@"[A-Za-z][Сс]|[Сс][A-Za-z]", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase),
+                new Regex(@"(?<!\\)[A-Za-z][Сс]|[Сс][A-Za-z]", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase),
                 new Regex(@"[А-Яа-яЁё][Cc]|[Cc][А-Яа-яЁё]", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase),
             })
         {
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ A Visual Studio Extension to highlight a russian letter С in an English word an
 ## Download 
 
 Here you can download this [vsix vor Visual Studio 2019](https://marketplace.visualstudio.com/items?itemName=lsoft.FixRussianC) or [vsix vor Visual Studio 2022](https://marketplace.visualstudio.com/items?itemName=lsoft.FixRussianC64).
+
+## Also
+
+You can download and install a [Roslyn analyzer](https://github.com/lsoft/FixRussianCAnalyzer) with the same checks. It will allow you to consider such warnings as errors and guard your project from such mistypings.
